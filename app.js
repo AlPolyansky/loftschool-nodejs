f9abbd2b6744ea1694d530ee6dacb9e97271df4a
Eclerrr <eclerrrr@yandex.ru>
2018-05-30 21:44:29 +0300

    create homehork-1

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-const fs = require('fs');
-const path = require('path');
- 
-var readFile = function(dir, done) {
+const fs = require("fs");
+const path = require("path");
+const del = require("del");
+const commander = require("commander");
+let rootFolder = path.resolve(__dirname, "root");
+
+const readFile = function(dir, done) {
   let results = [];
   fs.readdir(dir, function(err, list) {
-		if (err) return done(err);
-		
-		console.log(list);
- 
+    if (err) return done(err);
+
     let pending = list.length;
-		if (!pending) return done(null, results);
-		
+    if (!pending) return done(null, results);
+
     list.forEach(function(file) {
       file = path.resolve(dir, file);
       fs.stat(file, function(err, stat) {
@@ -21,17 +22,62 @@ var readFile = function(dir, done) {
           });
         } else {
           results.push({
-						name: path.parse(file).base,
-						path: file
-					});
+            name: path.parse(file).base,
+            path: file
+          });
           if (!--pending) done(null, results);
         }
       });
     });
   });
 };
- 
-readFile(path.resolve(__dirname, 'root'), (err, results) => {
+
+const sortAbc = function(a, b) {
+  if (a.name < b.name) return -1;
+  if (a.name > b.name) return 1;
+  return 0;
+};
+
+const createFolder = function(dirStr, callback = function() {}) {
+  if (!fs.existsSync(dirStr)) {
+    fs.mkdir(dirStr, (err, cb) => {
+      callback();
+    });
+  } else {
+    callback();
+  }
+};
+
+const putFileToFolder = function(item, newFolderPath, cb = function() {}) {
+  fs.readFile(item.path, (err, itemContent) => {
+    if (err) throw err;
+    fs.writeFile(`${newFolderPath}/${item.name}`, itemContent, err => {
+      if (err) throw err;
+      cb();
+    });
+  });
+};
+
+const createItemFolders = function(sortFolder, sortArr) {
+  sortArr.forEach(item => {
+    const newFolderPath = path.join(`${sortFolder}/${item.name[0]}`);
+    createFolder(newFolderPath, () => {
+      putFileToFolder(item, newFolderPath);
+    });
+  });
+};
+
+readFile(rootFolder, (err, results) => {
   if (err) throw err;
-  console.log(results);
+  const sortArr = results.sort(sortAbc);
+  const sortFolder = path.join(__dirname + "/sorted");
+
+  del([sortFolder, path.join(sortFolder + "/**/*")])
+    .then(path => {
+      createFolder(sortFolder, err => {
+        if (err) throw err;
+        createItemFolders(sortFolder, sortArr);
+      });
+    })
+    .catch(err => console.log(err));
 });
